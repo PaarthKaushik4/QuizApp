1b73b51035ba70252ec55ef0e69927bf071e4030
PaarthKaushik4 <161725723+PaarthKaushik4@users.noreply.github.com>
2025-07-31 19:56:43 +0530

    Update login.jsx

--- quiz-app-frontend/src/components/Auth/login.jsx
+++ quiz-app-frontend/src/components/Auth/login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
         setSuccess("");
 
         try {
-            const response = await fetch("http://localhost:5000/api/auth/login", {
+            const response = await fetch("https://quizapp-0dkr.onrender.com/api/auth/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ username: formData.username, password: formData.password }),
